85b3f6e1fddd65c36bd9c619fadb47f69ba77c8c
Tang233 <1825949538@qq.com>
2018-01-21 17:13:19 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+//import test from './test'
+import * as test from './test';
+
 ﻿const app =document.querySelector('#root')
 app.innerHTML='智安小可爱'
+
+console.log(test);
+
+console.log(test.default.num);
+console.log(test.default.foo());
+console.log(test.default.num);
--- src/test.js
+++ src/test.js
@@ -1,1 +1,23 @@
 console.log('your first module');
+//es6语法 模块化
+const num=20;
+const arr=[1,2,3,4];
+const obj={//定义对象
+  a:0,
+  b:function(){}
+}
+const foo=()=>{//定义函数
+  const a=0;
+  const b=20;
+  return a+b;
+}
+
+export default{//模块输出
+  num,
+  arr,
+  obj,
+  foo
+}
+
+export const bar=()=>{}
+export const zcar=12345;
