f05d6435a640ff80fc967532a8f50dfc0ce38cc9
Tang233 <1825949538@qq.com>
2018-02-06 01:15:42 +0800

    垂直水平居中

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // import test from './test'
 // import * as test from './test';
 import './index.css'
+var path= require('path')
 // import './signup.html'
 // ﻿const app =document.querySelector('#root')
 // app.innerHTML='智安小可爱'
 
 // console.log(test);
+var signup= function (){
+  // alert(1)
+  window.location.href='./signup.html';
+}
+// signup()
